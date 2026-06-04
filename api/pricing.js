8a8aa5798d1d48a7bc046d697942601aca4fa199
nurbajenglish-byte <nurbajenglish@gmail.com>
2026-06-04 09:17:14 +0700

    Remove Vercel serverless function

--- api/pricing.js
+++
@@ -1,5 +0,0 @@
-module.exports = function handler(req, res) {
-  const EARLY_BIRD_CUTOFF = new Date("2026-09-30T23:59:59Z");
-  const earlyBird = new Date() <= EARLY_BIRD_CUTOFF;
-  res.status(200).json({ earlyBird, cutoff: EARLY_BIRD_CUTOFF });
-};
